38c5df420a4725e767e824ed0ef7fdf48fd6a338
Deivison Nogueira <deivison.nogueira@live.com>
2022-09-25 21:19:07 -0400

    alter name

--- src/app/components/alunos/alunos.component.ts
+++ src/app/components/alunos/alunos.component.ts
@@ -5,16 +5,16 @@ import { MatTableDataSource } from '@angular/material/table';
 
 const alunosList: Aluno[] = [
 
-  new Aluno(1, 'Deivison', 10, 7, 9.7),
-  new Aluno(2, 'Kaique', 10, 7.8, 9.5),
-  new Aluno(3, 'Nobru', 9, 8, 8),
-  new Aluno(4, 'Robson', 15, 9, 3.5),
-  new Aluno(5, 'Isaque', 17, 10, 9),
-  new Aluno(6, 'Jeferson', 11, 9, 9),
-  new Aluno(7, 'Luis', 25, 6.5, 6),
-  new Aluno(8, 'Zaqueu', 14, 7, 7.5),
-  new Aluno(9, 'Saulo', 15, 7, 7.5),
-  new Aluno(10, 'Jamas', 16, 9, 9.5),
+  new Aluno(1, 'Deivison', 20, 7, 9.7),
+  new Aluno(2, 'Kaique', 26, 7.8, 9.5),
+  new Aluno(3, 'Nobru', 20, 8, 8),
+  new Aluno(4, 'Robson', 22, 9, 3.5),
+  new Aluno(5, 'Isaque', 15, 10, 9),
+  new Aluno(6, 'Jeferson', 20, 9, 9),
+  new Aluno(7, 'Luis', 20, 6.5, 6),
+  new Aluno(8, 'Zaqueu', 21, 7, 7.5),
+  new Aluno(9, 'Saulo', 28, 7, 7.5),
+  new Aluno(10, 'Jamas', 26, 9, 9.5),
 
 ]
 
@@ -32,7 +32,7 @@ export class AlunoComponent implements OnInit {
     { attribute: 'name', name: 'Nome', value: (element: Aluno) => `${element.name}` },
     { attribute: 'email', name: 'Email', value: (element: Aluno) => `${element.email}` },
     { attribute: 'age', name: 'Idade', value: (element: Aluno) => `${element.age}` },
-    
+
     { attribute: 'nota1', name: 'Nota1', value: (element: Aluno) => `${element.nota1}` },
     { attribute: 'nota2', name: 'Nota2', value: (element: Aluno) => `${element.nota2}` },
 
